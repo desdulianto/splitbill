3c7bb8e1046c2af86b2ae70302314f10e849d900
Des Dulianto <des.dulianto@bukalapak.com>
2018-09-12 14:36:29 +0700

    export Bill field name

--- splitbill.go
+++ splitbill.go
@@ -10,26 +10,26 @@ type Person string
 type People []Person
 
 type Bill struct {
-	amount Money  // total amount including tax (if applicable)
-	paidBy Person // person who paid the bill
-	people People // people in the group
+	Amount Money  // total amount including tax (if applicable)
+	PaidBy Person // person who paid the bill
+	People People // people in the group
 }
 
 // SplitEvenly, split the bill evenly by people counts (using integer division)
 // returns bill splitted evenly for each person
 func (bill Bill) SplitEvenly() (Money, error) {
-	if bill.amount <= Money(0) {
+	if bill.Amount <= Money(0) {
 		return Money(0), errors.New("Amount must not zero or negative")
 	}
-	return bill.amount / Money(len(bill.people)), nil
+	return bill.Amount / Money(len(bill.People)), nil
 }
 
 // GetPeople returns all the people names in the group excluding paidBy
 func (bill Bill) GetPeople() People {
 	var people People
 
-	for _, person := range bill.people {
-		if bill.paidBy != person {
+	for _, person := range bill.People {
+		if bill.PaidBy != person {
 			people = append(people, person)
 		}
 	}
